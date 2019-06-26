01c7ded19dc12df05d4bf20944c0b45d25fcb6ab
Marco.Foscato <mfo051@uib.no>
2019-06-26 10:00:28 +0200

    removing obsolete tests

---
+++ test/README.md
@@ -0,0 +1,3 @@
+# Tests for GM3DFRagmenter
+
+'WARNING' No test available for now.
--- test/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -f 3Dready* check-* MolFrag-ratio* targets.sdf others.sdf rejectedMols.sdf keptMols.sdf Fragments_* CPMap_* log_test*
-rm -rf fragsCollected*
-
